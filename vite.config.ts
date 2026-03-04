31620c3870cac549778ee64fae3d938bec2fff75
pmolina <patriciomolina@gmail.com>
2026-03-04 14:03:46 -0300

    Using custom domain deudas.ar

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/bcra/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })

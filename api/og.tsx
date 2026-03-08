fac49aa3367aa8c1602db8149aa10143c63c1858
pmolina <patriciomolina@gmail.com>
2026-03-07 21:49:13 -0300

    Adding CDN cache headers to prevent repeated hits to BCRA's API, which returns 429 and then 502.

--- api/og.tsx
+++ api/og.tsx
@@ -164,7 +164,8 @@ export default async function handler(req: Request) {
         width: 1200,
         height: 630,
         headers: {
-          'Cache-Control': 'public, max-age=3600, s-maxage=3600',
+          'Cache-Control': 'public, max-age=3600, s-maxage=86400, stale-while-revalidate=86400',
+          'CDN-Cache-Control': 'public, max-age=86400',
         },
       }
     );
